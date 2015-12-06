7a6b2369fa4a96f1cccc74660f402706d754a987
jer <me@jerlance.com>
2015-12-06 19:47:00 +0000

    Day 3, Puzzle 2

--- day03/Day03.php
+++ day03/Day03.php
@@ -5,12 +5,23 @@ class Day03 {
     private $_houses = 0;
     private $_deliveries = 0; 
     private $_world = array();
+    private $_roboDelivery = false;
 
     public function deliverPresents($path = NULL) { 
         $this->_resetWorld();   
         $this->_incrementLocation(0,0); //Always deliver where we are at least
 
+        if ($this->_roboDelivery) $this->_incrementLocation(0,0); // Robot santa must deliver
+
         if ($path) $this->_processInstructions($path);
+
+        return $this->_houses;
+    }
+
+    public function roboDeliverPresents($path = NULL) {
+        $this->_roboDelivery = true;
+        
+        $this->deliverPresents($path);
 
         return $this->_houses;
     }
@@ -21,32 +32,55 @@ class Day03 {
     }
 
     private function _processInstructions($path) {
-        $ns = 0;
-        $ew = 0;
+        $santa = array( 'ns' => 0, 'ew' => 0);
+        $robot = array( 'ns' => 0, 'ew' => 0);
 
         for ($i = 0; $i < strlen($path); ++$i) {
-            switch ($path[$i]) {
-                case '^':
-                    ++$ns;
-                    break;
-                case 'v':
-                    --$ns;
-                    break;
-                case '>':
-                    ++$ew;
-                    break;
-                case '<':
-                    --$ew;
-                    break;
+            if ($this->_getMover($i) == "santa") {
+                switch ($path[$i]) {
+                    case '^':
+                        ++$santa['ns'];
+                        break;
+                    case 'v':
+                        --$santa['ns'];
+                        break;
+                    case '>':
+                        ++$santa['ew'];
+                        break;
+                    case '<':
+                        --$santa['ew'];
+                        break;
+                }
+            
+                $this->_incrementLocation($santa['ns'], $santa['ew']);
+            } else {
+                switch ($path[$i]) {
+                    case '^':
+                        ++$robot['ns'];
+                        break;
+                    case 'v':
+                        --$robot['ns'];
+                        break;
+                    case '>':
+                        ++$robot['ew'];
+                        break;
+                    case '<':
+                        --$robot['ew'];
+                        break;
+                }
+                
+                $this->_incrementLocation($robot['ns'], $robot['ew']);
             }
 
-            $this->_incrementLocation($ns, $ew);
         }
     }
 
     private function _resetWorld() {
         unset($this->_world);
         $this->_world = array();
+
+        $this->_houses = 0;
+        $this->_deliveries = 0;
     }
 
     private function _incrementLocation($ns, $ew) {
@@ -57,10 +91,22 @@ class Day03 {
         ++$this->_deliveries;
     }
 
+    private function _getMover($n) {
+        $mover = "robot";
+        if (!$this->_roboDelivery) $mover = "santa";
+        if ($n % 2 == 0) $mover = "santa";
+
+        return $mover;
+    }
+
+
 }
 
 if (isset($argv[1])) {
     $day03 = new Day03;
-    $day03->deliverPresents($argv[1]);
+    if (isset($argv[2])) 
+        $day03->roboDeliverPresents($argv[1]); 
+    else 
+        $day03->deliverPresents($argv[1]);
     $day03->outputValues();
 }
--- day03/tests/Day03Tests.php
+++ day03/tests/Day03Tests.php
@@ -11,16 +11,22 @@ class Day03Tests extends PHPUnit_Framework_TestCase {
 
     function testEmptyPath() {
         $this->assertEquals(1, $this->_day03->deliverPresents());
+        $this->assertEquals(1, $this->_day03->roboDeliverPresents());
+
     }
 
     function testSingleInstruction() {
         $this->assertEquals(2, $this->_day03->deliverPresents('^'));
+        $this->assertEquals(2, $this->_day03->roboDeliverPresents('^'));
     }
 
     function testGivenInstructions() {
         $this->assertEquals(2, $this->_day03->deliverPresents('>'));
-        //$this->assertEquals(4, $this->_day03->deliverPresents('^>v<'));
-        //$this->assertEquals(2, $this->_day03->deliverPresents('^v^v^v^v^v'));
+        $this->assertEquals(4, $this->_day03->deliverPresents('^>v<'));
+        $this->assertEquals(2, $this->_day03->deliverPresents('^v^v^v^v^v'));
+        $this->assertEquals(3, $this->_day03->roboDeliverPresents('^v'));
+        $this->assertEquals(3, $this->_day03->roboDeliverPresents('^>v<'));
+        $this->assertEquals(11, $this->_day03->roboDeliverPresents('^v^v^v^v^v'));
     }
 
 }
